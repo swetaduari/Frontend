3018aba2be57500893c3b1d953df59644cb2ae57
swetaduari <161187872+swetaduari@users.noreply.github.com>
2026-03-29 20:01:31 +0530

    updated

--- Module-16) React - Fundamentals/3.Tasks/filter_by_Product_type/vite.config.js
+++ Module-16) React - Fundamentals/3.Tasks/filter_by_Product_type/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: process.env.VITE_BASE_PATH || '/Frontend/tree/main/Module-16)%20React%20-%20Fundamentals/3.Tasks/filter_by_Product_type',
 })
