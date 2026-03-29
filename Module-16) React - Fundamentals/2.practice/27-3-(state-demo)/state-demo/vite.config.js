62ae36ff612be66ce9bd1e6dd2f99fbfe8de54fb
swetaduari <161187872+swetaduari@users.noreply.github.com>
2026-03-29 19:44:14 +0530

    commit

--- Module-16) React - Fundamentals/2.practice/27-3-(state-demo)/state-demo/vite.config.js
+++ Module-16) React - Fundamentals/2.practice/27-3-(state-demo)/state-demo/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/Frontend/tree/main/Module-16)%20React%20-%20Fundamentals/2.practice/27-3-26(state-demo)/state-demo',
 })
